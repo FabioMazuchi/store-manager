332a8b7bd79b531e8d34f3ec8c90d412ced9f5c6
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-05-07 18:34:29 -0300

    corrige connection

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'docker',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
+
+// const connection = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'docker',
+//   database: 'StoreManager',
+// });
 
 module.exports = connection;
